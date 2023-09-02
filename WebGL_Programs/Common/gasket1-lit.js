c9d83b8baa59b8e776d780b8ecebaf5a6b0fdf1a
BuildTools <unconfigured@null.spigotmc.org>
2023-09-02 21:59:38 +0000

    Changed a little bit

---
+++ WebGL_Programs/Common/gasket1-lit.js
@@ -0,0 +1,69 @@
+/////////////////////////////////////////////////////////////////
+//    Sýnidæmi í Tölvugrafík
+//     Sýnir hvernig hægt er að breyta lit með uniform breytu
+//
+//    Hjálmtýr Hafsteinsson, ágúst 2023
+/////////////////////////////////////////////////////////////////
+var gl;
+var points;
+
+var colorLoc;
+
+window.onload = function init()
+{
+    var canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    //
+    //  Initialize our data for the Sierpinski Gasket
+    //
+
+    // First, initialize the corners of our gasket with three points.
+    
+    var vertices = new Float32Array([
+        -1, -1,
+        0,  1,
+        1, -1
+    ]);
+    
+
+    //
+    //  Configure WebGL
+    //
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    
+    //  Load shaders and initialize attribute buffers
+    
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+    
+    // Load the data into the GPU
+    
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW );
+
+    // Associate shader variables with our data buffer
+    
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    // Find the location of the variable fColor in the shader program
+    colorLoc = gl.getUniformLocation( program, "fColor" );
+
+    render();
+};
+
+
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    
+    gl.uniform4fv( colorLoc, vec4( Math.random(), Math.random(), Math.random(), 1.0 ));
+    gl.drawArrays( gl.TRIANGLES, 0, 6);
+
+}
---
+++ WebGL_Programs/Common/rectangle-tri-FAN.js
@@ -0,0 +1,60 @@
+//////////////////////////////////////////////////////////////////////
+//    Sýnidæmi fyrir heimadæmi 1 í Tölvugrafík
+//     Rétthyrningur teiknaður með tveimur sjálfstæðum þríhyrningum
+//
+//    Hjálmtýr Hafsteinsson, ágúst 2023
+//////////////////////////////////////////////////////////////////////
+var gl;
+var points;
+
+window.onload = function init()
+{
+    var canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+
+    /*var vertices = add( vec2( -0.5, 0.25 ), vec2( 0.5, 0.25 ), vec2( -0.5, 0.25 ), 
+                        vec2(-0.5, -0.25),  vec2(0.5, -0.25),  vec2(0.5, 0.25) );*/
+    
+    /*vertices = new Float32Array([-0.5, -0.25, 0.5, 0.25, -0.5, 0.25,
+								   -0.5, -0.25, 0.5, -0.25, 0.5, 0.25]);*/
+
+    // 2.a
+    vertices = new Float32Array([-0.5, 0.25, 0.5, 0.25, 0.5, -0.25, -0.5, -0.25]);
+
+    // 2.b
+    //vertices = new Float32Array([-0.5, 0.25, -0.5, -0.25, 0.5, 0.25, 0.5, -0.25]);
+
+    //  Configure WebGL
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    
+    //  Load shaders and initialize attribute buffers
+    
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+    
+    // Load the data into the GPU
+    
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW );
+
+    // Associate out shader variables with our data buffer
+    
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    render();
+};
+
+
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    //gl.drawArrays( gl.TRIANGLE_STRIP, 0, 6 ); // 2.b
+    gl.drawArrays( gl.TRIANGLE_FAN, 0, 6 ); // 2.a
+}
