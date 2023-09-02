346398ebfc82b9d1d9d5dfcfecdb0d25e2c5c8b1
BuildTools <unconfigured@null.spigotmc.org>
2023-09-02 22:56:00 +0000

    Verkefni part 4

--- WebGL_Programs/Common/gasket1-lit.js
+++ WebGL_Programs/Common/gasket1-lit.js
@@ -22,12 +22,24 @@ window.onload = function init()
 
     // First, initialize the corners of our gasket with three points.
     
-    var vertices = new Float32Array([
-        -1, -1,
-        0,  1,
-        1, -1
-    ]);
+    var vertices = new Float32Array(600);
     
+    for ( let i = 0; i < 600; i+=6 ){
+        // set location on screen
+        var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+        vertices[i] = plusOrMinus*Math.random();
+        plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+        vertices[i+1] = plusOrMinus*Math.random();
+        console.log(vertices);
+
+        // lower right making a triangle height 0.1 and width 0.1;
+        vertices[i+2] = vertices[i] - 0.05;     // x pos
+        vertices[i+3] = vertices[i+1] - 0.1;    // y pos
+
+        // lower left making a triangle height 0.1 and width 0.1;
+        vertices[i+4] = vertices[i] + 0.05;     // x pos
+        vertices[i+5] = vertices[i+1] - 0.1;    // y pos
+    } 
 
     //
     //  Configure WebGL
@@ -63,7 +75,9 @@ window.onload = function init()
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
     
-    gl.uniform4fv( colorLoc, vec4( Math.random(), Math.random(), Math.random(), 1.0 ));
-    gl.drawArrays( gl.TRIANGLES, 0, 6);
+    for ( let i = 0; i < 300; i+=3 ){
+        gl.uniform4fv( colorLoc, vec4( Math.random(), Math.random(), Math.random(), 1.0 ));
+        gl.drawArrays( gl.TRIANGLES, i, 3 );
+    }
 
 }
