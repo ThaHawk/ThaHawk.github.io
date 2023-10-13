55aeb37ac983b600e66cfdf151e10afb7a1b7b71
ThaHawk <arnoringi88@hotmail.com>
2023-10-13 10:24:17 +0000

    Breytti nafninu á Heimaverkefni yfir í Heimadaemi

---
+++ WebGL_Programs/Heimadaemi_4/d1/triangle.js
@@ -0,0 +1,86 @@
+///////////////////////////////////////////////////////////////////
+//    Sýnidæmi í Tölvugrafík
+//    Einfaldasta WebGL forritið.  Teiknar einn rauðan þríhyrning.
+//
+//    Hjálmtýr Hafsteinsson, ágúst 2023
+///////////////////////////////////////////////////////////////////
+var gl;
+var points;
+
+var color = vec4( 1.0, 0.0, 0.0, 1.0 );
+var locColor;
+var matrixLoc;
+var iniTime;
+var theta = 0.0;
+
+window.onload = function init()
+{
+    var canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    var vertices = [    
+        vec2( -0.5, -0.5 ), 
+        vec2( 0.0, 0.5 ), 
+        vec2( 0.5, -0.5 )
+    ];
+
+    //  Configure WebGL
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 0.95, 1.0, 1.0, 1.0 );
+    
+    //  Load shaders and initialize attribute buffers
+    
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+    
+    // Load the data into the GPU
+    
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+
+    // Associate shader variables with our data buffer
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    
+    locColor = gl.getUniformLocation( program, "rcolor" );
+    gl.uniform4fv( locColor, flatten(color) );
+
+    iniTime = Date.now();
+
+    matrixLoc = gl.getUniformLocation( program, "translation" );
+    
+    render();
+};
+
+
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+
+    // add some translation
+    var mv = mat4();
+    mv = mult( mv, rotateZ( theta ) );
+    theta += 0.5;
+    gl.uniformMatrix4fv(matrixLoc, false, flatten(mv));
+
+    
+    var msek = Date.now() - iniTime; // milliseconds since we started program
+    //console.log(Math.floor(msek / 1000)); // second counter
+    if (Math.floor(msek / 1000) % 2)
+        color = vec4( 1.0, 1.0, 1.0, 1.0 );
+    else
+        color = vec4( 1.0, 0.0, 0.0, 1.0 );
+    
+    gl.uniform4fv(locColor, flatten(color)); // send color to fragment shader
+    
+    gl.uniform4fv( locColor, flatten(color) );
+
+    gl.drawArrays( gl.TRIANGLES, 0, 3 ); // draw triangle
+    
+    window.requestAnimFrame(render);
+}
