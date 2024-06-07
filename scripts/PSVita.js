d69f3ae71e8a8c6990a7b0e22ced445f062fbd79
ThaHawk <arnoringi88@hotmail.com>
2024-06-07 15:37:40 +0000

    PSVita Wallpaper project added

---
+++ scripts/PSVita.js
@@ -0,0 +1,5 @@
+import { PSVitaDB } from "./PSVitaDB.js";
+var size = 0;
+for (const item of PSVitaDB){size += 1;}
+
+
---
+++ scripts/PSVitaDB.js
@@ -0,0 +1,26 @@
+export const PSVitaDB = [
+  {
+    gameName:"Bit Trip Runner", 
+    fileName:"PSVitaBG-Bit-Trip-Runner.png"
+  },
+  {
+    gameName:"God of War Collection", 
+    fileName:"PSVitaBG-God-Of-War.png"
+  },
+  {
+    gameName:"Gravity Rush", 
+    fileName:"PSVitaBG-GravityRush.png"
+  },
+  {
+    gameName:"Killzone Mercenary", 
+    fileName:"PSVitaBG-Killzone-Mercenary-v2.png"
+  },
+  {
+    gameName:"Little Big Planet", 
+    fileName:"PSVitaBG-LittleBigPlanet-v2.jpg"
+  },
+  {
+    gameName:"Persona 4 GOLDEN", 
+    fileName:"PSVitaBG-Persona-4-GOLDEN.png"
+  }
+];
