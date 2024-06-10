1ee8696bcff0e2a195b7cc2b8bada40efc2a2541
ThaHawk <arnoringi88@hotmail.com>
2024-06-10 22:16:09 +0000

    Now Reading Stays open

--- bokasafnid/bookcase.js
+++ bokasafnid/bookcase.js
@@ -1,3 +1,4 @@
+// fetch json library
 const response = await fetch('./bokasafnid.json');
 const books = await response.json();
 
@@ -6,24 +7,36 @@ const books = await response.json();
 const body = document.querySelector("body");
 
 // list of objects
-
 var arrayOfYears = [];
 
-// loop through each object and check year finished reading
-// add object
+// global variable
 let count = 0;
+let index
+
+// reverse array so most recently read books are on the top
+books.reverse();
+
 
 books.forEach(function(obj){
   // create and add div to an array to be displayed later
+  // example a text that says "2024"
   let element = document.createElement("h1");
   arrayOfYears.push(element);
 
   // count how many items in the array
   count = arrayOfYears.length;
-  let index = count - 1;
+  index = count - 1;
 
-  // take year from date object
-  let year = new Date(obj.end).getFullYear();
+  let year = "";
+
+  // if there is no end date. The book 
+  // is assumed to be in progres.
+  if (obj.end === null){
+    year = "NowReading";
+  }
+  else {
+    year = new Date(obj.end).getFullYear();
+  }
 
   // if year div doesn't exist, create it,
   // and append to body.
@@ -34,6 +47,7 @@ books.forEach(function(obj){
     bookcase.id = "year" + year;
 
     arrayOfYears[index].className = "showBooks";
+    arrayOfYears[index].id = "h1-" + year;
     arrayOfYears[index].textContent = year;
     arrayOfYears[index].addEventListener("click", function(){
       bookcase.classList.toggle("visible");
@@ -45,6 +59,8 @@ books.forEach(function(obj){
 
   // add things to the div
   const basicBox      = document.createElement("div");
+  const textBox       = document.createElement("div");
+  const imgBox        = document.createElement("div");
   const bookName      = document.createElement("h2");
   const bookAuthor    = document.createElement("h3");
   const bookWordCount = document.createElement("p");
@@ -55,20 +71,29 @@ books.forEach(function(obj){
   const bookImg       = document.createElement("img");
 
   basicBox.className          = "basicBox";
+  textBox.className           = "textBox";
+  imgBox.className            = "imgBox";
   bookName.textContent        = obj.name;
-  bookAuthor.textContent      = obj.author;
-  bookWordCount.textContent   = obj.wordCount;
-  bookRating.textContent      = obj.rating;
-  bookDesc.textContent        = obj.desc;
-  bookStart.textContent       = obj.start;
-  bookEnd.textContent         = obj.end;
-  bookImg.src                 = "img/" + obj.fileName;
+  bookAuthor.textContent      = "Höfundur: "    + obj.author;
+  bookWordCount.textContent   = "Orðafjöldi: "  + obj.wordCount;
+  bookRating.textContent      = "Stig: "        + obj.rating + "/5";
+  bookDesc.textContent        = "Lýsing: "      + obj.desc;
+  bookStart.textContent       = "Byrjaði: "     + obj.start;
+  bookEnd.textContent         = "Kláraði: "     + obj.end;
+  bookImg.src                 = "img/"          + obj.fileName;
   
-  bookcase = document.querySelector("#year" + year)
-  bookcase.appendChild(basicBox);
-  basicBox.appendChild(
-    bookImg, bookName, bookAuthor, bookWordCount, bookRating,
-    bookDesc, bookStart, bookEnd, bookImg
+  
+  bookcase = document.querySelector("#year" + year);
+  bookcase.append( basicBox );
+  basicBox.append( imgBox , textBox );
+  imgBox.append(bookImg);
+  textBox.append(
+    bookName, bookAuthor, bookWordCount, bookRating,
+    bookDesc, bookStart, bookEnd
   );
+  
 
 });
+
+// fix now reading section
+document.querySelector("#h1-NowReading").textContent = "Currently Reading";
