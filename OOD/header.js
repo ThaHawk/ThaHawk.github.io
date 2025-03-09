f3a334c75097fd021e67f93216911ef90c9d605f
thahawk <arnoringi88@hotmail.com>
2025-03-09 23:02:04 +0000

    big change on home

--- OOD/header.js
+++ OOD/header.js
@@ -1,51 +1,17 @@
-function toggleClass() {
-  let classText = "." + "myMenu";
-  const menu = document.querySelector(classText);
-  menu.classList.toggle("visible");
-  //console.log("Attempted to toggle " + classText);
-}
-
-/*<header>
+/*
+<header>
   <a class="title">
     <img src="../img/watermark-white.png">
   </a>
   <a class="menu-toggle" onclick="toggleClass('myMenu')">
     <img src="../img/Hamburger_White.png">
   </a>
-</header>*/
-const header = document.createElement("header");
-{
-  const body = document.querySelector("body");
-  const topDiv = document.createElement("div");
-  const title = document.createElement("a");
-  const menuToggle = document.createElement("a");
-  const watermark = document.createElement("img");
-  const hamburger = document.createElement("img");
-  
-  topDiv.className = "top";
-  
-  title.className = "title";
-  title.href = "../home";
-  
-  menuToggle.className = "menu-toggle";
-  menuToggle.addEventListener("click", toggleClass);
+</header>
+*/
 
 
-  watermark.src = "../img/watermark-white.png";
-  hamburger.src = "../img/Hamburger_White.png";
-  
-  body.prepend(topDiv);
-  topDiv.append(header);
-  header.append(title);
-  header.append(menuToggle);
-  title.append(watermark);
-  menuToggle.append(hamburger);
-}
-
-
-
-
-/*<nav id="myMenu" class="myMenu">
+/*
+<nav id="myMenu" class="myMenu">
   <ul>
     <li>
       <a class="navButton" href="../minecraft">
@@ -53,54 +19,99 @@ const header = document.createElement("header");
       </a>
     </li>
   </ul>
-</nav>*/
-const nav = document.createElement("nav");
-const unOrderedList = document.createElement("ul");
+</nav>
+*/
 
-nav.id = "myMenu";
-nav.className = "myMenu";
 
-header.after(nav);
-nav.append(unOrderedList);
 
-var listOfSites = [
-  {
-    site:"Heim",
-    path:"../home"
-  },
-  {
-    site:"Bókasafnið",
-    path:"../bokasafnid"
-  },
-  {
-    site:"PSVita",
-    path:"../PSVita"
-  },
-  {
-    site:"Minecraft",
-    path:"../minecraft"
-  },
-  {
-    site:"Ljósmyndun",
-    path:"../ljosmyndun"
-  },
-  {
-    site:"Viðgerðir",
-    path:"../vidgerdir"
-  },
-  {
-    site:"Teachings",
-    path:"../teachings"
-  }
-];
 
-listOfSites.forEach(function(obj){
-  const tempListItem = document.createElement("li");
-  const tempA = document.createElement("a");
+newHeader();
 
-  tempA.text = obj.site + " --|";
-  tempA.href = obj.path;
+function newHeader()  {
+  // create HTML elements
+  const header = document.createElement("header");
+  const body = document.querySelector("body");
+  const topDiv = document.createElement("div");
+  const title = document.createElement("a");
+  const menuToggle = document.createElement("a");
+  const watermark = document.createElement("img");
+  const hamburger = document.createElement("img");
+  const nav = document.createElement("nav");
+  const ul = document.createElement("ul");
 
-  unOrderedList.append(tempListItem);
-  tempListItem.append(tempA);
-});
+  // assign class names and id so that css can find them
+  topDiv.className = "top";
+  title.className = "title";
+  menuToggle.className = "menu-toggle";
+  nav.id = "myMenu";
+
+  // causes the navigation on and off. See below list of items
+  menuToggle.addEventListener("click", function () {
+    const menu = document.querySelector("nav");
+    menu.classList.toggle("visible");
+  });
+
+  // pressing the logo sends you to the homepage
+  title.href = "../home";
+
+  watermark.src = "../img/watermark-white.png";
+  hamburger.src = "../img/Hamburger_White.png";
+
+  // top div is the parent div and needs to be first in body. thus prepend
+  body.prepend(topDiv); 
+
+  topDiv.append(header);
+  header.append(title);
+  header.append(menuToggle);
+  title.append(watermark);
+  menuToggle.append(hamburger);
+  header.after(nav);
+  nav.append(ul);
+
+
+  // I could maybe move this to a json but for now its here.
+  var listOfSites = [
+    {
+      site:"Heim",
+      path:"../home"
+    },
+    {
+      site:"Bókasafnið",
+      path:"../bokasafnid"
+    },
+    {
+      site:"PSVita",
+      path:"../PSVita"
+    },
+    {
+      site:"Minecraft",
+      path:"../minecraft/UltimateBodyPart"
+    },
+    {
+      site:"Ljósmyndun",
+      path:"../ljosmyndun"
+    },
+    {
+      site:"Viðgerðir",
+      path:"../vidgerdir"
+    },
+    {
+      site:"Teachings",
+      path:"../teachings"
+    }
+  ];
+
+  listOfSites.forEach(function(obj){
+
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+
+    a.text = obj.site;
+    a.text += " --|"; // the is just for flare and can be removed.
+    a.href = obj.path;
+
+    ul.append(li);
+    li.append(a);
+  });
+ 
+}
